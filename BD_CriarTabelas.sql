c9af9a4c6ccce765a0fe0f67e60ae874391d874c
felipequecole <felipequecole@msn.com>
2018-04-11 00:19:35 -0300

    Corrigindo detalhes

--- BD_CriarTabelas.sql
+++ BD_CriarTabelas.sql
@@ -1,11 +1,15 @@
+drop table Promocao; 
+drop table Hotel;
+drop table Site;
+
 create table Site
 (
-url varchar(256) not null,
-nome varchar(256) not null,
-email varchar(256) not null,
-telefone varchar(24) not null,
-senha varchar(256) not null,
-CONSTRAINT primary_key PRIMARY KEY (url)
+    url varchar(256) not null,
+    nome varchar(256) not null,
+    email varchar(256) not null,
+    telefone varchar(24) not null,
+    senha varchar(256) not null,
+    CONSTRAINT site_primary_key PRIMARY KEY (url)
 );
 
 create table Hotel
@@ -13,16 +17,17 @@ create table Hotel
     cnpj varchar(256) not null,
     nome varchar(256) not null,
     cidade varchar(256) not null, 
-    senha varchar(256) not null
-    CONSTRAINT primary_key PRIMARY KEY (cnpj)
+    senha varchar(256) not null,
+    CONSTRAINT hotel_primary_key PRIMARY KEY (cnpj)
 );
 
 create table Promocao
 (
     id integer not null generated always as identity (start with 1, increment by 1),
-    url varchar(256) references Site(url),
-    cnpj varchar(256) references Hotel(cnpj),
+    url varchar(256) references Site(url) on delete cascade,
+    cnpj varchar(256) references Hotel(cnpj) on delete cascade,
     data_inicio Date not null, 
     data_fim Date not null,
-    preco Decimal not null
+    preco Decimal not null,
+    CONSTRAINT promocao_primary_key PRIMARY KEY (id)
 ); 
