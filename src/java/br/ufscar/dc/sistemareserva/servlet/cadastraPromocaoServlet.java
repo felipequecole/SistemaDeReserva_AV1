b7837a330ceb2ced04afe67c959ead5a142a28df
felipequecole <felipequecole@msn.com>
2018-04-18 02:02:26 -0300

    eu to com sono e fome

--- src/java/br/ufscar/dc/sistemareserva/servlet/cadastraPromocaoServlet.java
+++ src/java/br/ufscar/dc/sistemareserva/servlet/cadastraPromocaoServlet.java
@@ -42,21 +42,21 @@ public class cadastraPromocaoServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    
     @Resource(name = "jdbc/SistemaReservaDBLocal")
     DataSource datasource;
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException, ParseException{
+            throws ServletException, IOException, ParseException {
         response.setContentType("text/html;charset=UTF-8");
         CadastraPromocaoFormBean cpfb = new CadastraPromocaoFormBean();
         List<String> mensagens = new ArrayList<String>();
+        String cnpj = (String) request.getSession().getAttribute("cnpj"); 
         try {
             BeanUtils.populate(cpfb, request.getParameterMap());
-            mensagens = cpfb.validar();
-            request.getSession().setAttribute("form",cpfb);
+            mensagens = cpfb.validar(datasource, cnpj);
+            request.getSession().setAttribute("form", cpfb);
             Promocao promocao = new Promocao();
-            promocao.setCnpj((String) request.getSession().getAttribute("cnpj"));
+            promocao.setCnpj(cnpj);
             System.out.println(promocao.getCnpj());
             SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
             try {
@@ -64,35 +64,36 @@ public class cadastraPromocaoServlet extends HttpServlet {
                 promocao.setData_fim(sdf.parse(cpfb.getData_fim()));
                 promocao.setPreco(cpfb.getPreco());
                 promocao.setUrl(cpfb.getUrl());
-                if (mensagens != null){
-                       request.setAttribute("mensagem", mensagens);
-                       request.getSession().setAttribute("form",cpfb);
-                       request.getRequestDispatcher("cadastraPromocao.jsp").forward(request,response);                   
-                }
-                PromocaoDAO pdao = new PromocaoDAO(datasource);
-                Promocao promocao_ret = null;
-                try {
-                    promocao_ret = pdao.gravaPromocao(promocao);
-                    
-                } catch (SQLException ex) {
-                    Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
-                    request.setAttribute("mensagem", "Erro ao acessar o banco.");
-                    request.getRequestDispatcher("erro.jsp").forward(request, response);
-                }
-                if (promocao_ret != null) {
-                    request.setAttribute("mensagem", "Promoção cadastrada com sucesso!");
-                    response.sendRedirect("index.jsp");
+                if (mensagens != null) {
+                    request.setAttribute("mensagem", mensagens);
+                    request.getSession().setAttribute("form", cpfb);
+                    request.getRequestDispatcher("cadastraPromocao.jsp").forward(request, response);
                 } else {
-                    request.setAttribute("mensagem", "Erro ao salvar no banco");
-                    request.getRequestDispatcher("erro.jsp").forward(request, response);
+                    PromocaoDAO pdao = new PromocaoDAO(datasource);
+                    Promocao promocao_ret = null;
+                    try {
+                        promocao_ret = pdao.gravaPromocao(promocao);
+                        if (promocao_ret != null) {
+                            request.setAttribute("mensagem", "Promoção cadastrada com sucesso!");
+                            response.sendRedirect("index.jsp");
+                        } else {
+                            request.setAttribute("mensagem", "Erro ao salvar no banco");
+                            request.getRequestDispatcher("erro.jsp").forward(request, response);
+                        }
+
+                    } catch (SQLException ex) {
+                        Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
+                        request.setAttribute("mensagem", "Erro ao acessar o banco.");
+                        request.getRequestDispatcher("erro.jsp").forward(request, response);
+                    }
+
                 }
             } catch (ParseException ex) {
                 Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
                 request.setAttribute("mensagem", mensagens);
                 request.getRequestDispatcher("cadastraPromocao.jsp").forward(request, response);
             }
-           
-            
+
         } catch (IllegalAccessException ex) {
             Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
             request.getSession().setAttribute("mensagem", ex.getLocalizedMessage());
@@ -102,7 +103,7 @@ public class cadastraPromocaoServlet extends HttpServlet {
             request.getSession().setAttribute("mensagem", ex.getLocalizedMessage());
             request.getRequestDispatcher("erro.jsp").forward(request, response);
         }
-        
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -121,8 +122,8 @@ public class cadastraPromocaoServlet extends HttpServlet {
             processRequest(request, response);
         } catch (ParseException ex) {
             Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.getSession().setAttribute("mensagem","Eduardo é um pau no cu get");
-            request.getRequestDispatcher("erro.jsp").forward(request, response);           
+            request.getSession().setAttribute("mensagem", "Eduardo é um pau no cu get");
+            request.getRequestDispatcher("erro.jsp").forward(request, response);
         }
     }
 
@@ -141,8 +142,8 @@ public class cadastraPromocaoServlet extends HttpServlet {
             processRequest(request, response);
         } catch (ParseException ex) {
             Logger.getLogger(cadastraPromocaoServlet.class.getName()).log(Level.SEVERE, null, ex);
-            request.getSession().setAttribute("mensagem","Eduardo é um pau no cu post");
-            request.getRequestDispatcher("erro.jsp").forward(request, response); 
+            request.getSession().setAttribute("mensagem", "Eduardo é um pau no cu post");
+            request.getRequestDispatcher("erro.jsp").forward(request, response);
         }
     }
 
